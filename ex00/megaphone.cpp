c241231dc2a12ebbaf7c3ed930e3780d7d1b0ecb
xamayuel <xamayuel@Portatil>
2024-02-22 18:15:04 +0100

    primer ejercicio

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 18:06:31 by xamayuel          #+#    #+#             */
+/*   Updated: 2024/02/22 18:07:21 by xamayuel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char* argv[])
+{
+    int i(1);
+	int j;
+
+	if (argc > 1)
+	{
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+				std::cout << (char)toupper(argv[i][j++]);
+			std::cout << " ";
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (0);
+}
