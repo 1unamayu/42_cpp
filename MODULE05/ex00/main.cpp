f4a0e121961b0c55337c3e80cd29b18053b80e35
xamayuel Mint <unamayu@gmail.com>
2024-03-20 18:44:51 +0100

    mas module05

--- MODULE05/ex00/main.cpp
+++ MODULE05/ex00/main.cpp
@@ -6,23 +6,33 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 15:42:51 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/03/20 17:04:04 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:43:48 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-// FALTA HACER MAIN
-int main()
+void makeFish()
 {
-  std::cout << KRED "-----------------------" KNOR << std::endl;
-  std::cout << KRED "MODULE 04 - Exercise:00" KNOR << std::endl;
-  std::cout << KRED "-----------------------" KNOR << std::endl;
   std::cout << LBLU "               O  o" << std::endl;
   std::cout << KCYA "          _\\_ " LBLU "  o" << std::endl;
   std::cout << KPUR ">('>" KCYA "   \\\\/  o\\" LBLU " . " << std::endl;
   std::cout << KCYA "       //\\___=" << std::endl;
   std::cout << std::endl;
+}
+void makeHeader(const std::string& moduleText) {
+ int lineLength = moduleText.length() + 2; // Adjust for extra spaces
+ std::string line = std::string(lineLength, '-');
+
+ std::cout << KRED << line << KNOR << std::endl;
+ std::cout << KRED << " " << moduleText << " " << KNOR << std::endl;
+ std::cout << KRED << line << KNOR << std::endl;
+}
+// FALTA HACER MAIN
+int main()
+{
+  makeFish();
+  makeHeader("MODULE 05 - Exercise 00 - Mommy, when I grow up, I want to be a bureaucrat!");
 
   Bureaucrat bure1("Beñat", 2);
   Bureaucrat bure2("Elian", 149);
--- MODULE05/ex01/main.cpp
+++ MODULE05/ex01/main.cpp
@@ -6,23 +6,33 @@
 /*   By: xamayuel <xamayuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 15:42:51 by xamayuel          #+#    #+#             */
-/*   Updated: 2024/03/20 17:21:54 by xamayuel         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:42:54 by xamayuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
+void makeFish()
+{
+  std::cout << LBLU "               O  o" << std::endl;
+  std::cout << KCYA "          _\\_ " LBLU "  o" << std::endl;
+  std::cout << KPUR ">('>" KCYA "   \\\\/  o\\" LBLU " . " << std::endl;
+  std::cout << KCYA "       //\\___=" << std::endl;
+  std::cout << std::endl;
+}
+void makeHeader(const std::string& moduleText) {
+ int lineLength = moduleText.length() + 2; // Adjust for extra spaces
+ std::string line = std::string(lineLength, '-');
+
+ std::cout << KRED << line << KNOR << std::endl;
+ std::cout << KRED << " " << moduleText << " " << KNOR << std::endl;
+ std::cout << KRED << line << KNOR << std::endl;
+}
 // FALTA HACER MAIN
 int main()
 {
-    std::cout << KRED "-----------------------" KNOR << std::endl;
-    std::cout << KRED "MODULE 04 - Exercise:01" KNOR << std::endl;
-    std::cout << KRED "-----------------------" KNOR << std::endl;
-    std::cout << LBLU "               O  o" << std::endl;
-    std::cout << KCYA "          _\\_ " LBLU "  o" << std::endl;
-    std::cout << KPUR ">('>" KCYA "   \\\\/  o\\" LBLU " . " << std::endl;
-    std::cout << KCYA "       //\\___=" << std::endl;
-    std::cout << std::endl;
+    makeFish();
+    makeHeader("MODULE 05 - Exercise 01 - Form up, maggots!");
 
     Bureaucrat bure1("Beñat", 2);
 }
